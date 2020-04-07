6d764dbbf7571ffd6d46070a84426adb89686528
steals <steals@me.com>
2020-04-07 18:47:14 +0300

    Updated snapcast to 0.18.1

--- packages/addons/addon-depends/snapcast-depends/snapcast/package.mk
+++ packages/addons/addon-depends/snapcast-depends/snapcast/package.mk
@@ -2,12 +2,12 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="snapcast"
-PKG_VERSION="0.15.0"
-PKG_SHA256="7c584fad4941a299339fe060174e33c4d810b1cbe80d6efbee54da3dafb252cc"
+PKG_VERSION="0.18.1"
+PKG_SHA256="425afb7e24768ca08c247dc394aaa25fd2a6886e6789e18c6f024eabd7e0f688"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/badaix/snapcast"
 PKG_URL="https://github.com/badaix/snapcast/archive/v$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain aixlog alsa-lib asio avahi flac libvorbis popl"
+PKG_DEPENDS_TARGET="toolchain aixlog alsa-lib avahi flac libvorbis opus"
 PKG_LONGDESC="Synchronous multi-room audio player."
 PKG_TOOLCHAIN="make"
 
