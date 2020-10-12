3b2f4f083a7107515a27613480bd66179a3972a0
steals <steals@me.com>
2020-10-12 18:27:40 +0300

    Restored aceproxy version

--- packages/xmedia/torrent/aceproxy/package.mk
+++ packages/xmedia/torrent/aceproxy/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2011-present Alex@ELEC (http://alexelec.in.ua)
 
 PKG_NAME="aceproxy"
-PKG_VERSION="e5fc845"
-PKG_SHA256="3671ecce8f1b8c4925570f6d48ec6b0c086903241e02878f3f03cb285aa4146b"
+PKG_VERSION="2561431"
+PKG_SHA256="6238623e0e9e4a5fe4c41261dbffeb954725fe0a879ccb7e8b26c0a75bedc4cc"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/AndreyPavlenko/aceproxy"
 PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
