ab52af971627a732c3d8d644572b997b62be4617
steals <steals@me.com>
2020-10-13 08:49:26 +0300

    Updated TvHeadend

--- packages/xmedia/tvservice/tvh/package.mk
+++ packages/xmedia/tvservice/tvh/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2011-present Alex@ELEC (http://alexelec.in.ua)
 
 PKG_NAME="tvh"
-PKG_VERSION="6be300c"
+PKG_VERSION="c4d086c"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.tvheadend.org"
 PKG_URL=""
